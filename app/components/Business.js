4599d6c44ab0701b4ebf8a8176ae56c8c6fc3914
lorecodez <133832488+lorecodez@users.noreply.github.com>
2023-05-21 18:19:08 -0700

    setup

---
+++ app/components/Business.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Business(){
+    const image = {src:'', alt:''};
+    const name = '';
+    const address = '';
+    const city = '';
+    const state = '';
+    const zipcode = '';
+    const category = '';
+    const rating = '';
+    const reviewCount = null; 
+    
+
+
+}
+
+export default Business;
